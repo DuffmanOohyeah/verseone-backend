d09f65b5cd4693b558bde02a67882c993dfb1cdf
DuffmanOohyeah <uk@booMedia.ca>
2025-05-16 23:22:20 +0100

    Tidy test endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,20 @@
 import express, { Express, Request, Response } from 'express';
 import compression from 'compression';
 import cors from 'cors';
+import { v4 as uuidv4 } from 'uuid';
+
+interface ThingProps {
+	[key: string]: string;
+}
+
+interface RezProps {
+	status: number;
+	json: {};
+}
 
 const app: Express = express();
 const port: string | number = process.env.PORT || 3001;
+let thingObj: ThingProps = {};
 
 app.use(compression());
 
@@ -21,12 +32,33 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Basic route
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response): void => {
 	res.send('Hello, World!');
 });
 
-app.get('/api/confirm', (req: Request, res: Response) => {
+app.get('/api/confirm', (req: Request, res: Response): void => {
 	res.json({ message: 'Yay! Successfully connected to Node.js' });
+});
+
+app.post('/post/thing', (req: Request, res: Response): void => {
+	const { body } = req;
+	let rez: RezProps = { status: 400, json: { error: 'No data provided' } };
+	if (body) {
+		thingObj[uuidv4()] = body;
+		rez = { status: 200, json: thingObj };
+	}
+	res.status(rez.status).json(rez.json);
+});
+
+app.get('/get/things', (req: Request, res: Response): void => {
+	res.status(200).json(thingObj);
+});
+
+app.get('/get/thing/:id', (req: Request, res: Response): void => {
+	const { id } = req.params;
+	let rez: RezProps = { status: 400, json: { error: 'Thing not found' } };
+	if (id && thingObj[id]) rez = { status: 200, json: thingObj[id] };
+	res.status(rez.status).json(rez.json);
 });
 
 // Start the server
